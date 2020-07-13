b0efe6b8691c4d4fdc8fcf49227d9c0b56dd6b29
correyananta <Correyananta.adhilaksma@gmail.com >
2020-07-13 13:40:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,33 @@ const updateScreen = (number) => {
 
 const number = document.querySelectorAll(".number")
 
-numbers.foreach((number) => {
+numbers.forEach((number) => {
   number.addEventListener("click", (event) => {
+    inputNumber(currentNumber)
     updateScreen(event.target.value)
   })
 })
+
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = '0'
+
+const inputNumber = (number) => {
+  if (currentNumber === '0') {
+    currentNumber = number
+  } else {
+    currentNumber += number
+  }
+}
+const operators = document.querySelectorAll(".operator")
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value)
+  })
+})
+
+const inputOperator = (operator) => {
+  prevNumber = currentNumber
+  calculationOperator = operator
+  currentNumber = ''
+}
